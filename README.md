4fa921616df1bb4ac32a307f32b786d1df544173
Gumeece <81268663+SamMcph@users.noreply.github.com>
2022-08-07 01:15:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vlrWebScrapper
+ A web scrapper that grabs the ranking of the top valorant esport teams  in each region. 
---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+class rankings():
+    def na_ranking():
+        r = requests.get('https://www.vlr.gg/rankings/north-america/')
+        
+        soup = BeautifulSoup(r.content, 'html.parser')
+        list_of_teams = []
+        for i in soup.findAll('div', class_='ge-text'):
+            list_of_teams.append(str(i.get_text(",", strip=True)))
+        team_rankings ={}
+        for i in range(len(list_of_teams)):
+            list_of_teams[i]= list_of_teams[i].split(',')
+            team_rankings[i]= list_of_teams[i][0]
+        return team_rankings
+    def eu_ranking():
+        r = requests.get('https://www.vlr.gg/rankings/europe')
+        
+        soup = BeautifulSoup(r.content, 'html.parser')
+        list_of_teams = []
+        for i in soup.findAll('div', class_='ge-text'):
+            list_of_teams.append(str(i.get_text(",", strip=True)))
+        team_rankings ={}
+        for i in range(len(list_of_teams)):
+            list_of_teams[i]= list_of_teams[i].split(',')
+            team_rankings[i]= list_of_teams[i][0]
+        return team_rankings
+    def brazil_ranking():
+        r = requests.get('https://www.vlr.gg/rankings/brazil')
+        soup = BeautifulSoup(r.content, 'html.parser')
+        list_of_teams = []
+        for i in soup.findAll('div', class_='ge-text'):
+            list_of_teams.append(str(i.get_text(",", strip=True)))
+        team_rankings ={}
+        for i in range(len(list_of_teams)):
+            list_of_teams[i]= list_of_teams[i].split(',')
+            team_rankings[i]= list_of_teams[i][0]
+        return team_rankings
+    def asia_pacific_ranking():
+        r = requests.get('https://www.vlr.gg/rankings/asia-pacific')
+        soup = BeautifulSoup(r.content, 'html.parser')
+        list_of_teams = []
+        for i in soup.findAll('div', class_='ge-text'):
+            list_of_teams.append(str(i.get_text(",", strip=True)))
+        team_rankings ={}
+        for i in range(len(list_of_teams)):
+            list_of_teams[i]= list_of_teams[i].split(',')
+            team_rankings[i]= list_of_teams[i][0]
+        return team_rankings   
+
+
+
+team_rankings = rankings()
+
+print(rankings.asia_pacific_ranking())
